e343873d2132342d87e71851680e62299526a73d
Jarod Wilson <jaroddwilson@gmail.com>
2022-01-06 15:13:47 -0500

    commit tree 676e8d74509db12dda7ba637f34ca6cdc0b58d03

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return reverseWord(word) === word;
+}
+
+function reverseWord(word) {
+  return word.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  Create a function to reverse a word
+  Compare the reversed word with the original word
+  Return true if they match, false if not
 */
 
 /*
   Add written explanation of your solution here
+  It takes an input of a word, converts it to an array, reverses the array, then combines the resulting array into a string.
+  The resulting string is compared with the original word.
+  If they match, the function returns true; otherwise false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
